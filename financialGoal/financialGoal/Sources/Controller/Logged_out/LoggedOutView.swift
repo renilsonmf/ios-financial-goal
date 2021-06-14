daa2df3bac85529878e0331366e6d0eeddeaceb1
Renilson <renilson.moreiraf@gmail.com>
2021-06-14 16:02:35 -0300

    feat(EFG-36): Criando arquivos para criação da tela inicial

---
+++ financialGoal/financialGoal/Sources/Controller/Logged_out/LoggedOutView.swift
@@ -0,0 +1,8 @@
+//
+//  LoggedOutView.swift
+//  financialGoal
+//
+//  Created by Renilson Moreira Ferreira on 14/06/21.
+//
+
+import Foundation
---
+++ financialGoal/financialGoal/Sources/Controller/Logged_out/LoggedOutViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoggedOutViewController.swift
+//  financialGoal
+//
+//  Created by Renilson Moreira Ferreira on 14/06/21.
+//
+
+import UIKit
+
+class LoggedOutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
